6f2650471a90c53e154b7307fdb189ca5f3e2bbb
ABOY-IA <arnaud.boy.ia@gmail.com>
2024-09-10 13:11:05 +0200

    save

--- Test Python/test.py
+++ Test Python/test.py
@@ -2,10 +2,36 @@ import numpy
 
 from numpy.random import rand
 
-toto = 1
-while toto > 0.5:
-    print(toto)
-    toto = rand()
+# toto = 1
+# while toto > 0.5:
+#     print(toto)
+#     toto = rand()
 
-print("fin", toto)
+# print("fin", toto)
+
+a = input("écris: ")
+# print(a)
+# print(type(a))
+# print(len(a))
+
+# for indice in range(len(a)):
+#     print(a[indice])
+
+# for carac in a:
+#     print(carac)
+
+list_numeric = list() #déclarer list python2
+list_pasnumeric = [] #déclarer list python3
+
+for carac in a:
+    if carac.isnumeric():
+        list_numeric.append(int(carac)*1.0)
+        list_numeric.append(float(carac))
+    else:
+        list_pasnumeric.append(carac)
+
+print("")
+print("="*20)
+print("Numeric", list_numeric)
+print("Pas numeric", list_pasnumeric)
 
